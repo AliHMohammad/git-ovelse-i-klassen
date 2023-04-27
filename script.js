0a70450820c9be04d52b4249ca16a44056e2bff3
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-04-27 13:18:01 +0200

    chrisFunction created

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ window.addEventListener("load", start);
 
 function start(event) {
     console.log("Hello Ali");
+    chrisFunction();
+}
+
+function chrisFunction () {
+    console.log("Chris was here!");
 }
